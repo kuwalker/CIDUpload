1189e743a5709eba4985e8a9b1e18c9943274bc7
Naveen0503 <109223016+Naveen0503@users.noreply.github.com>
2023-05-30 18:34:44 +0530

    created the api

---
+++ CID Upload/Controllers/CIDupload.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CID_Upload.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CIDupload : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Upload(IFormFile file)
+        {
+            try
+            {
+                if (file == null || file.Length == 0)
+                    return BadRequest("No file selected.");
+
+                string currentDirectory = Directory.GetCurrentDirectory();
+                string uploadsFolder = Path.Combine(currentDirectory, "images");
+                if (!Directory.Exists(uploadsFolder))
+                    Directory.CreateDirectory(uploadsFolder);
+
+                string uniqueFileName =  DateTime.Now.ToShortDateString()+ file.FileName ; 
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                string imageUrl = Path.Combine(currentDirectory, "images", uniqueFileName);
+
+                return Ok(new { imageUrl });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error while uploading the file: " + ex.Message);
+            }
+        }
+    }
+
+    }
