82c4d3b315eea22b5bb99fe091136e7ec2910c30
Naveen0503 <194079.gvp@gmail.com>
2023-05-31 19:13:08 +0530

    update

--- CID Upload/Controllers/CIDupload.cs
+++ CID Upload/Controllers/CIDupload.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.IO;
 
 namespace CID_Upload.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class CIDupload : ControllerBase
-    {
+    {        [HttpPost]
+        public IActionResult Post([FromBody] string imageUrl)
+        {
+            using (var client = new WebClient())
+            {
+                // Download the image from the provided URL
+                byte[] imageBytes = client.DownloadData(imageUrl);
+
+                // Create the images folder if it doesn't exist
+                string imagesFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "images");
+                if (!Directory.Exists(imagesFolderPath))
+                {
+                    Directory.CreateDirectory(imagesFolderPath);
+                }
+
+                // Generate a unique filename for the image
+                string imageFileName = Path.GetFileNameWithoutExtension(imageUrl) + "_" + Path.GetRandomFileName() + Path.GetExtension(imageUrl);
+                string imagePath = Path.Combine(imagesFolderPath, imageFileName);
+
+                // Save the image to the images folder
+                System.IO.File.WriteAllBytes(imagePath, imageBytes);
+
+                return Ok(new { imagePath });
+            }
+        }
         [HttpPost]
         public IActionResult Upload(IFormFile file)
         {
@@ -20,7 +46,7 @@ namespace CID_Upload.Controllers
                 if (!Directory.Exists(uploadsFolder))
                     Directory.CreateDirectory(uploadsFolder);
 
-                string uniqueFileName =  DateTime.Now.ToShortDateString()+ file.FileName ; 
+                string uniqueFileName =   file.FileName ; 
                 string filePath = Path.Combine(uploadsFolder, uniqueFileName);
 
                 using (var stream = new FileStream(filePath, FileMode.Create))
