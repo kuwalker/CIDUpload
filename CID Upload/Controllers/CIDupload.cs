a0b5ee770adf8e3e5172bf9b58c7578746b8260a
Naveen0503 <109223016+Naveen0503@users.noreply.github.com>
2023-06-01 19:37:59 +0530

    final

--- CID Upload/Controllers/CIDupload.cs
+++ CID Upload/Controllers/CIDupload.cs
@@ -1,84 +1,61 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System.Net;
+﻿using System;
 using System.IO;
-using MimeTypeMap;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace CID_Upload.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    
     public class CIDupload : ControllerBase
-    {   
-             [HttpPost("CID2")]
-        public IActionResult Post([FromBody] string imageUrl)
+    {
+        private readonly IWebHostEnvironment _hostEnvironment;
+        private readonly IConfiguration _configuration;
+
+        public CIDupload(IWebHostEnvironment hostEnvironment, IConfiguration configuration)
         {
-           
-           using (var client = new WebClient())
-            {
-                // Download the image from the provided URL
-                byte[] imageBytes = client.DownloadData(imageUrl);
-
-                // Determine the image extension based on the image data
-                string imageExtension = MimeTypeMap.GetExtension(imageBytes);
-
-                // If the extension is not found or not a known image format, use a default extension like ".jpg"
-                if (string.IsNullOrEmpty(imageExtension) || !imageExtension.StartsWith("."))
-                {
-                    imageExtension = ".jpg";
-                }
-
-                // Create the images folder if it doesn't exist
-                string imagesFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "images");
-                if (!Directory.Exists(imagesFolderPath))
-                {
-                    Directory.CreateDirectory(imagesFolderPath);
-                }
-
-                // Generate a unique filename with the original name and extension
-                string imageFileName = Path.GetFileName(new Uri(imageUrl).LocalPath) + imageExtension;
-                string imagePath = Path.Combine(imagesFolderPath, imageFileName);
-
-                // Save the image to the images folder
-                using (var imageFileStream = new FileStream(imagePath, FileMode.Create))
-                {
-                    imageFileStream.Write(imageBytes, 0, imageBytes.Length);
-                }
-
-                return Ok(new { imagePath });
-            }
+            _hostEnvironment = hostEnvironment;
+            _configuration = configuration;
         }
+
         [HttpPost]
-        public IActionResult Upload(IFormFile file)
+        public async Task<IActionResult> UploadImageFromUrl([FromForm] string imageUrl)
         {
             try
             {
-                if (file == null || file.Length == 0)
-                    return BadRequest("No file selected.");
-
-                string currentDirectory = Directory.GetCurrentDirectory();
-                string uploadsFolder = Path.Combine(currentDirectory, "images");
-                if (!Directory.Exists(uploadsFolder))
-                    Directory.CreateDirectory(uploadsFolder);
-
-                string uniqueFileName =   file.FileName ; 
-                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-
-                using (var stream = new FileStream(filePath, FileMode.Create))
+                using (HttpClient client = new HttpClient())
                 {
-                    file.CopyTo(stream);
+                    var response = await client.GetAsync(imageUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var stream = await response.Content.ReadAsStreamAsync();
+                        var fileName = Path.GetFileName(imageUrl);
+
+                        string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=supportcentralsa;AccountKey=df1j1BL8ENXILlreJYCHOyGGtgi3+Z42uuTHPc3LeYnCw1Us3HTlDsXx4qPUknORo8CSEDgXLWdt+AStU8H7Dw==;EndpointSuffix=core.windows.net";
+                        BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
+                        BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("images");
+
+                        BlobClient blobClient = containerClient.GetBlobClient(fileName);
+                        await blobClient.UploadAsync(stream, true);
+
+                        string uploadedImageUrl = blobClient.Uri.ToString();
+
+                        return Ok(new { imageUrl = uploadedImageUrl });
+                    }
+
+                    return BadRequest("Unable to download the image from the specified URL.");
                 }
-
-                string imageUrl = Path.Combine(currentDirectory, "images", uniqueFileName);
-
-                return Ok(new { imageUrl });
             }
             catch (Exception ex)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError, "Error while uploading the file: " + ex.Message);
+                return StatusCode(500, $"An error occurred: {ex.Message}");
             }
         }
     }
-
-    }
+}
